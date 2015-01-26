5f097784455a5804f81bf9db22bb83395be75b1e
cokeboL <165128167@qq.com>
2015-01-26 16:52:57 +0800

    +-

--- src/Classes/Rotate.cpp
+++ src/Classes/Rotate.cpp
@@ -5,13 +5,20 @@ GLfloat	rotate = 0.02f;;
 GLfloat xMove = 0.005;
 GLfloat yMove = 0.0;
 GLfloat zMove = 0.0;
+bool flag = false;
 
 void drawGLScene(GLvoid)						
 {
 	xMove = -xMove;
 	glTranslatef(xMove, yMove, zMove);
 
-	glRotatef(rotate, 0.0f, 1.0f, 0.0f);
+	glRotatef(rotate, 0.5f, 0.5f, 0.0f);
+
+	if (!flag)
+	{
+		glScalef(0.5, 0.3, 0.8);
+		flag = true;
+	}
 
 	glBegin(GL_TRIANGLES);
 
