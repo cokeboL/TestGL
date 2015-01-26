3d2adc9476c590a0a321a84c157f58391db21202
cokeboL <165128167@qq.com>
2015-01-26 16:43:13 +0800

    -

--- src/Classes/Rotate.cpp
+++ src/Classes/Rotate.cpp
@@ -2,25 +2,14 @@
 #include "Rotate.h"
 
 GLfloat	rotate = 0.02f;;
-GLfloat xMove = 0.1;
+GLfloat xMove = 0.005;
 GLfloat yMove = 0.0;
 GLfloat zMove = 0.0;
-GLfloat inter = 0.01;
 
 void drawGLScene(GLvoid)						
 {
-	xMove = xMove + inter;
-	if (xMove > 0.3)
-	{
-		xMove = 0.3;
-		inter = -inter;
-	}
-	if (xMove < -0.3)
-	{
-		xMove = -0.3;
-		inter = -inter;
-	}
-	//glTranslatef(xMove, yMove, zMove);
+	xMove = -xMove;
+	glTranslatef(xMove, yMove, zMove);
 
 	glRotatef(rotate, 0.0f, 1.0f, 0.0f);
 
